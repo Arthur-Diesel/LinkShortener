d6f5fefbd589dba5e84c3238e7e6ce6c1ae2a92c
Arthur Diesel Ogg <arthurdiesel@outlook.com>
2024-04-15 08:11:02 -0300

    feat: Adicionando rotas para aplicação.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import { debugApi } from "./config/constants";
+import router from "./routes";
 
-const app = express();
 const port = process.env.PORT || 3000;
 
+const app = express();
+app.use(express.json());
+app.use(router);
+
 app.listen(port, () => {
-    debugApi(`Server is running on port ${port}.`);    
+  debugApi(`Server is running on port ${port}.`);
 });
